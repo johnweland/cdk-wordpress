7dab77eb1e33e193fd687fa453bbfd6a31c9615c
John Weland <john.weland@gmail.com>
2023-07-20 15:21:18 -0500

    does not deploy! service tasks cannot mount efs

--- lib/wordpress-cdk-stack.ts
+++ lib/wordpress-cdk-stack.ts
@@ -37,17 +37,18 @@ export class WordpressCdkStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           cidrMask: 24,
         },
-      ]
+      ],
+      natGateways: 1,
     });
 
-    const fileSystem = new efs.FileSystem(this, `${stackname}-fs`, {
-      vpc: vpc,
-      fileSystemName: 'wordpress-filesystem',
-      vpcSubnets: vpc.selectSubnets({
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-      }),
-      encrypted: true,
-    });
+    // const fileSystem = new efs.FileSystem(this, `${stackname}-fs`, {
+    //   vpc: vpc,
+    //   fileSystemName: 'wordpress-filesystem',
+    //   vpcSubnets: vpc.selectSubnets({
+    //     subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+    //   }),
+    //   encrypted: true,
+    // });
 
     const cluster = new ecs.Cluster(this, `${stackname}-cluster`, {
       vpc: vpc,
@@ -56,21 +57,25 @@ export class WordpressCdkStack extends cdk.Stack {
       capacity: {
         instanceType: new ec2.InstanceType('t3.micro'),
         desiredCapacity: 1,
+        vpcSubnets: vpc.selectSubnets({
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }),
       },
     });
 
-    const taskDefinition = new ecs.Ec2TaskDefinition(this, `${stackname}-task-def`, {
+    const taskDefinition = new ecs.TaskDefinition(this, `${stackname}-task-def`, {
       networkMode: ecs.NetworkMode.AWS_VPC,
+      compatibility: ecs.Compatibility.EC2,
     });
 
-    taskDefinition.addVolume({
-      name: 'wordpress-efs',
-      efsVolumeConfiguration: {
-        fileSystemId: fileSystem.fileSystemId,
-        transitEncryption: 'ENABLED',
-        rootDirectory: '/',
-      },
-    });
+    // taskDefinition.addVolume({
+    //   name: 'wordpress-efs',
+    //   efsVolumeConfiguration: {
+    //     fileSystemId: fileSystem.fileSystemId,
+    //     transitEncryption: 'ENABLED',
+    //     rootDirectory: '/',
+    //   },
+    // });
 
     const container = taskDefinition.addContainer(`${stackname}-container`, {
       image: ecs.ContainerImage.fromRegistry('public.ecr.aws/bitnami/nginx:latest'), // 'public.ecr.aws/bitnami/wordpress:latest'
@@ -83,11 +88,11 @@ export class WordpressCdkStack extends cdk.Stack {
       }),
     });
 
-    container.addMountPoints({
-      containerPath: '/', // '/var/www/html',
-      sourceVolume: 'wordpress-efs',
-      readOnly: false,
-    });
+    // container.addMountPoints({
+    //   containerPath: '/', // '/var/www/html',
+    //   sourceVolume: 'wordpress-efs',
+    //   readOnly: false,
+    // });
 
     const service = new ecs.Ec2Service(this, `${stackname}-service`, {
       cluster: cluster,
